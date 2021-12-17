bab72f7849317103043679a7d5c34b10b2d46f88
EunjungSong <strange.ssong75@gmail.com>
2021-12-17 14:35:04 +0900

    api key 부분 삭제

--- src/App.js
+++ src/App.js
@@ -6,17 +6,7 @@ function App() {
   const [videos, setVideos] = useState([]);
 
   useEffect(() => {
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow' // fetch를 쓸 때, 리퀘스트를 쓸 때,
-    };
-    
-    fetch("https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyBDyBbSMfvK2T0jpYuj9E_W1RS6NO5fcqc",
-     requestOptions
-    )
-      .then(response => response.json())
-      .then(result => setVideos(result.items))
-      .catch(error => console.log('error', error));
+
   }, []);
 
   
