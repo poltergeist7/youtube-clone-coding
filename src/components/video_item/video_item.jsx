7519ef45c1ce0df1a504e2868a8d065ac3bfc13f
EunjungSong <strange.ssong75@gmail.com>
2021-12-22 09:12:28 +0900

    리스트 아이템

--- src/components/video_item/video_item.jsx
+++ src/components/video_item/video_item.jsx
@@ -1,9 +1,54 @@
 import React from 'react';
+import styled from 'styled-components';
 
-const VideoItem = (props) => {
+const VideoItem = ({video: {snippet}}) => {
     return (
-        <h1>{props.video.snippet.title}</h1>
+        <ListItem>
+            <Video>
+                <img src={snippet.thumbnails.medium.url} alt="video thumbnails" />
+                <div>
+                    <Title>{snippet.title}</Title>
+                    <Channel>{snippet.channelTitle}</Channel>
+                </div>
+            </Video>
+        </ListItem>
     );
 };
 
 export default VideoItem;
+
+const ListItem = styled.li`
+    width: 50%;
+    cursor: pointer;
+    padding: 0.2rem;
+`;
+
+const Video = styled.div`
+    display: flex;
+    align-items: center;
+    border: 1px solid lightblue;
+    box-shadow: 2px 2px 5px 0px rgba(191, 191, 191, 0.53);
+    transition: transform 250ms ease-in;
+
+    $:hover{
+        transform: scale(1.02);
+    }
+
+    img {
+        width: 40%;
+        height: 100%;
+    }
+    
+    div {
+        margin-left: 0.2rem
+        
+    }
+`;
+
+const Title =styled.p`
+    font-size: 0.8rem;
+`;
+
+const Channel = styled.p`
+    font-size: 0.6rem;
+`;
--- src/components/video_list/video_list.jsx
+++ src/components/video_list/video_list.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import VideoItem from '../video_item/video_item';
+import styled from 'styled-components';
 
 const VideoList = (props) => {
     return (
-        <ul>
+        <VideosUl>
             {props.videos.map(video => 
                 (<VideoItem key={video.id} video={video} />
             ))}
-        </ul>
+        </VideosUl>
     );
 };
 
 export default VideoList;
+
+const VideosUl = styled.ul`
+    display: flex;
+    flex-wrap: wrap;
+    list-style: none;
+    padding-left: 0;
+`;
